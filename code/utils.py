abc416c60c5964e4681ccb07d6d2b2f2a2d9e2d2
Marvin Buss <marvin.buss@gmail.com>
2020-03-20 12:11:10 +0100

    lint

--- code/utils.py
+++ code/utils.py
@@ -10,8 +10,8 @@ def required_parameters_provided(parameters, keys, message="Required parameter n
     missing_keys = []
     for key in keys:
         if key not in parameters:
-            err_msg = f"{message} {key}" 
+            err_msg = f"{message} {key}"
             print(f"::error::{err_msg}")
-            missing_keys.append(key) 
+            missing_keys.append(key)
     if len(missing_keys) > 0:
         raise AMLConfigurationException(f"{message} {missing_keys}")
