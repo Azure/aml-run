9ee1319f03ba5050ed4e93157fdf3afeb35ac452
awmatheson <awmatheson@github.com>
2020-04-06 10:23:21 -0400

    update markdown function

--- code/utils.py
+++ code/utils.py
@@ -18,28 +18,35 @@ def required_parameters_provided(parameters, keys, message="Required parameter n
 
 
 def convert_to_markdown(metrics_dict):
-    markdown = ""
-    for k in metrics_dict.keys():
-        markdown += f"## {k} %0A%0A"
+    
+    exp = list(metrics_dict.keys())
 
-        # format headers
-        headers = "|"
-        for nam in metrics_dict[k].keys():
-            headers += f" {nam} |"
-        markdown += headers + "%0A"
+    experiment = exp[0]
 
-        # add lines under headers
-        markdown += "|" + " -- |" * len(metrics_dict[k]) + "%0A"
+    # check if multiple runs and remove the first if so since it is a summary.
+    if len(exp) > 1:
+        runs = exp[1:]
+    else: runs = exp
 
-        # add values
-        metrics = "|"
-        for val in metrics_dict[k].values():
+    # add comment header
+    markdown = f"## Experiment Run Details:%0A%0A Run ID: {experiment} %0A%0A"
+
+    # build table header 
+    markdown += "| Run ID | Parameter | Value |%0A| ----- | ----- | ----- |%0A"
+    for run in runs:
+        # add metrics and values
+        for k, val in metrics_dict[run].items():
+            row = f"| {run} | {k} |"
             try:
                 val = float(val)
-                metrics += f" {val:.3} |"
+                row += f" {val:.3} |"
             except ValueError:
-                metrics += f" {val} |"
-        markdown += metrics + "%0A"
+                row += f" {val} |"
+            except TypeError:
+                row += f" {val} |"
+            markdown += row + "%0A"
+
+    return markdown
 
     return markdown
 
