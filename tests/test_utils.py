3732f11d0151ff291835e0b49e09456bc1562459
Marvin Buss <marvin.buss@gmail.com>
2020-04-30 17:41:20 +0200

    moved test related pieces out of code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,13 +6,43 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(myPath, "..", "code"))
 
 from utils import AMLConfigurationException, AMLExperimentConfigurationException, convert_to_markdown, validate_json, load_pipeline_yaml, load_runconfig_yaml, load_runconfig_python
-from schemas import azure_credentials_schema, parameters_schema, markdown_conversion_input, markdown_out
+from schemas import azure_credentials_schema, parameters_schema
 
 
 def test_markdown_single_experiment_conversion():
     """
     Unit test to check the markdown conversion
     """
+    markdown_conversion_input = {
+        "5815b172-61fe-4e8d-baa2-e4ffe33e77d3": {
+            "testkey": "testvalue"
+        },
+        "HD_7034e363-3172-41e6-8423-eb292f39e233": {
+            "best_child_by_primary_metric": {
+                "metric_name": ["mse", "mse"],
+                "timestamp": ["2020-04-06 16:04:23.719570+00:00", "2020-04-06 16:04:23.719570+00:00"],
+                "run_id": ["HD_7034e363-3172-41e6-8423-eb292f39e233_1", "HD_7034e363-3172-41e6-8423-eb292f39e233_1"],
+                "metric_value": [0.025010755222666936, 0.025010755222666936],
+                "final": [False, True]
+            }
+        },
+        "HD_7034e363-3172-41e6-8423-eb292f39e233_0": {
+            "auc": 0.6394267984578837,
+            "mse": 0.025010755222666936,
+            "TimeSeries comparison": "aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_0/TimeSeries comparison_1586189163.png"
+        },
+        "HD_7034e363-3172-41e6-8423-eb292f39e233_1": {
+            "auc": 0.6394267984578837,
+            "mse": 0.025010755222666936,
+            "TimeSeries comparison": "aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_1/TimeSeries comparison_1586189032.png"
+        },
+        "HD_7034e363-3172-41e6-8423-eb292f39e233_2": {
+            "mse": 0.025010755222666936,
+            "auc": 0.6394267984578837,
+            "TimeSeries comparison": "aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_2/TimeSeries comparison_1586189099.png"
+        }
+    }
+    markdown_out = """## Run Details:%0A%0A Experiment/Pipeline ID: 5815b172-61fe-4e8d-baa2-e4ffe33e77d3 %0A%0A| Run ID | Parameter | Value |%0A| ----- | ----- | ----- |%0A| 5815b172-61fe-4e8d-baa2-e4ffe33e77d3 | testkey | testvalue |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_0 | auc | 0.639 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_0 | mse | 0.025 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_0 | TimeSeries comparison | aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_0/TimeSeries comparison_1586189163.png |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_1 | auc | 0.639 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_1 | mse | 0.025 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_1 | TimeSeries comparison | aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_1/TimeSeries comparison_1586189032.png |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_2 | mse | 0.025 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_2 | auc | 0.639 |%0A| HD_7034e363-3172-41e6-8423-eb292f39e233_2 | TimeSeries comparison | aml://artifactId/ExperimentRun/dcid.HD_7034e363-3172-41e6-8423-eb292f39e233_2/TimeSeries comparison_1586189099.png |%0A"""
     markdown = convert_to_markdown(
         markdown_conversion_input
     )
